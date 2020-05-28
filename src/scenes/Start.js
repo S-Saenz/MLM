a7c6d4c5ada395a6a3d5c6e0330835279f9c7498
S-Saenz <sarsaenz@ucsc.edu>
2020-05-27 21:30:38 -0700

    create start scene

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -7,12 +7,23 @@ class Start extends Phaser.Scene {
     preload() {
 
         //this.load.image('audioOff', '././assets/audioOff.png');
-        this.load.image('start_bg', '././assets/login_bg.png');
+        this.load.image('start_bg', '././assets/coverImageFull.png');
+        this.load.image('login_bg', '././assets/login_bg.png');
 
         
         this.load.image('loginButton', '././assets/loginButton.png');
         this.load.image('loginButtonHover', '././assets/loginButtonHover.png');
 
+        
+        //hover sounds
+        this.load.audio('hover1SFX', '././assets/sfx/hover1.wav');
+        this.load.audio('hover2SFX', '././assets/sfx/hover2.wav');
+        this.load.audio('hover3SFX', '././assets/sfx/hover3.wav');
+        this.load.audio('hover4SFX', '././assets/sfx/hover4.wav');
+        //click sounds
+        this.load.audio('click1SFX', '././assets/sfx/click1.wav');
+        this.load.audio('click2SFX', '././assets/sfx/click2.wav');
+        this.load.audio('click3SFX', '././assets/sfx/click3.wav');
 
     }
 
@@ -22,22 +33,22 @@ class Start extends Phaser.Scene {
 
         
         //up down buttons
-        this.loginButton = this.add.image(game.config.width-100,game.config.height-50, 'loginButton').setScale(0.5).setDepth(4);
+        this.loginButton = this.add.image(centerX,centerY+100, 'loginButton').setScale(0.5).setDepth(1);
 
 
+        
+        this.timer = this.time.delayedCall(2000, () => {
+            this.fadeOut();
+        }, null, this);
 
 
-        this.bg = this.add.tileSprite(0, 0, 960, 650, 'start_bg').setOrigin(0, 0);
+        this.coverImg = this.add.tileSprite(0, 0, 960, 650, 'start_bg').setOrigin(0, 0).setDepth(3);
+        this.bg = this.add.tileSprite(0, 0, 960, 650, 'login_bg').setOrigin(0, 0);
 
 
         this.loginButton.setInteractive();
         this.loginButton.on('pointerdown', () => { 
-            if(this.bg.y>=-1480){
-                this.bg.height+=100;
-                this.bg.y-=100;
-
-            }
-            console.log(this.bg.y);
+            this.scene.start("messengerScene");
         });
 
         this.loginButton.on('pointerover', () => { 
@@ -54,5 +65,13 @@ class Start extends Phaser.Scene {
     update() {
     }
 
+    fadeOut(){
+        if(this.coverImg.alpha > 0){
+            this.timer = this.time.delayedCall(50, () => {
+                this.coverImg.alpha -= 0.1;
+                this.fadeOut();
+            }, null, this);
+        }
+    }
     
 }
